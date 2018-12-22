f101c2179103b277e014e14f7b8e6125c4a501a9
An Huynh <huynhhongan2908@gmail.com>
2018-12-22 23:00:01 +0700

    complete menu layout

--- app/src/main/java/com/nmcnpm/nhom10/moneylover/MainActivity.java
+++ app/src/main/java/com/nmcnpm/nhom10/moneylover/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nmcnpm.nhom10.moneylover;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_transaction) {
+//            Intent intent = new Intent(this, RegisterActivity.class);
+//            startActivity(intent);
+        } else if (id == R.id.nav_wallets) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_bills) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_account_setting) {
 
         }
 
