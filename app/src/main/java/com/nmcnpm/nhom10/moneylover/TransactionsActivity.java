690dba8cbb72524c19915b117816d9c822e6d7d0
An Huynh <huynhhongan2908@gmail.com>
2018-12-24 03:04:33 +0700

    init listview in "Transactions"

--- app/src/main/java/com/nmcnpm/nhom10/moneylover/TransactionsActivity.java
+++ app/src/main/java/com/nmcnpm/nhom10/moneylover/TransactionsActivity.java
@@ -20,13 +20,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TransactionsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
+    ArrayList<TransactionModel> dataModels;
+    ListView listView;
+    private static TransactionAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +73,38 @@ public class TransactionsActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        listView=(ListView)findViewById(R.id.lvTransaction);
+
+        dataModels= new ArrayList<>();
+
+        dataModels.add(new TransactionModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
+        dataModels.add(new TransactionModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
+        dataModels.add(new TransactionModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
+        dataModels.add(new TransactionModel("Donut","Android 1.6","4","September 15, 2009"));
+        dataModels.add(new TransactionModel("Eclair", "Android 2.0", "5","October 26, 2009"));
+        dataModels.add(new TransactionModel("Froyo", "Android 2.2", "8","May 20, 2010"));
+        dataModels.add(new TransactionModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
+        dataModels.add(new TransactionModel("Honeycomb","Android 3.0","11","February 22, 2011"));
+        dataModels.add(new TransactionModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
+        dataModels.add(new TransactionModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
+        dataModels.add(new TransactionModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
+        dataModels.add(new TransactionModel("Lollipop","Android 5.0","21","November 12, 2014"));
+        dataModels.add(new TransactionModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
+
+        adapter= new TransactionAdapter(dataModels,getApplicationContext());
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TransactionModel dataModel= dataModels.get(position);
+
+                Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+            }
+        });
     }
 
     @Override
